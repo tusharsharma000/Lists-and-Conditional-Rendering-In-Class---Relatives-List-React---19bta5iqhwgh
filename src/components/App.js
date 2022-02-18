7f1541a7e0c67b966839af7ceef4d2ac64aace79
tusharsharma000 <84301919+tusharsharma000@users.noreply.github.com>
2022-02-18 23:12:03 +0530

    String

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,26 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  constructor() {
+    super();
+    this.state = {
+      name: ["Gaourav", "Tushar", "Shruti", "Nishi"]
+    };
+  }
+  render() {
+    return (
+      <div id="main">
+        <ol key="relativeListItem">
+          {this.state.name &&
+            this.state.name.map((item, index) => {
+              let temp = "[relativeListItem" + { index };
+              return <li key={temp}>{item}</li>;
+            })}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
